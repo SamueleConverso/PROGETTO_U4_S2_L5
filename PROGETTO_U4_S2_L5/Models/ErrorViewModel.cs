f6e0ab2680a62cf11b2c427aa739ab5e89f81d0a
Samuele Converso <samu.converso@gmail.com>
2025-02-21 09:18:08 +0100

    Aggiungere i file di progetto.

---
+++ PROGETTO_U4_S2_L5/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROGETTO_U4_S2_L5.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
